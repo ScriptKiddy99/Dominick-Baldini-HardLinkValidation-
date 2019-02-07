1369f241926edbe546ffc749a800fd625cfaf8fa
ScriptKiddy99 <47433174+ScriptKiddy99@users.noreply.github.com>
2019-02-07 22:13:54 +0000

    Initial Version
    This will check if all movie under E:\Plex\Movies has a hard link and that hard link target location is in E:\deluge\Data, if not will prompt the user if they want to delete the file. By default ANY file that does not have a hardlink will be deleted.

---
+++ Remove-HardLink.ps1
@@ -0,0 +1,30 @@
+﻿$FilesHL  = Get-ChildItem -Recurse -Path "E:\Plex\Movies" | Where-Object {$_.Mode.toLower() -notmatch "d" -and $_.LinkType -ne "HardLink"}
+$FilesWHL = Get-ChildItem -Recurse -Path "E:\Plex\Movies" | Where-Object {$_.Mode.toLower() -notmatch "d" -and $_.LinkType -eq "HardLink"}
+
+ForEach($File in $FilesHL)
+{
+    if($File.LinkType -ne "HardLink")
+    {
+        Remove-Item -Path $File.FullName -Force
+    }
+}
+
+ForEach($File in $FilesWHL)
+{
+    if([Bool]($File.Target -notmatch 'E:\\deluge\\Data'))
+    {
+        Write-Host -ForegroundColor Red "$($File.FullName) has an invalid link would you like to delete Y\N"
+
+        $Response = Read-Host
+
+        If($Response.ToLower() -eq "y")
+        {
+            Remove-Item -Path $FIle.FullName -Force 
+            Remove-Item -Path $File.Directory.FullName -Force
+        }
+    }
+    else
+    {
+        Write-Host -ForegroundColor Green "$($File.FullName) has an valid link"
+    }
+}
